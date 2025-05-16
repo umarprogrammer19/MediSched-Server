7dff72dd14c1132152d529338c48227a600a5cb8
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-05-16 16:32:48 +0500

    Auth Handler Update:

--- auth/auth_handler.py
+++ auth/auth_handler.py
@@ -1,6 +1,10 @@
 from passlib.context import CryptContext
 from fastapi_jwt_auth import AuthJWT
 from pydantic import BaseModel
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -13,9 +17,8 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
 
-# JWT config
 class Settings(BaseModel):
-    authjwt_secret_key: str = "YOUR_SECRET_KEY"  # Use env variable in prod
+    authjwt_secret_key: str = os.getenv("JWT_SECRET_KEY")
 
 
 @AuthJWT.load_config

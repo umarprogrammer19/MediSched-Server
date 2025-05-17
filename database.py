b324566ab2023a706a5b6d3922bde4afa12c2713
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-05-17 15:32:16 +0500

    Updated Database Models

--- database.py
+++ database.py
@@ -1,6 +1,11 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from beanie import init_beanie
 from models.user import User
+from models.doctor_details import (
+    DoctorDetails,
+) 
+from models.appointment import Appointment
+from models.message import Message
 import os
 from dotenv import load_dotenv
 
@@ -9,10 +14,11 @@ load_dotenv()
 MONGODB_URI = os.getenv("MONGODB_URI")
 
 client = AsyncIOMotorClient(MONGODB_URI)
-
 db = client["medisched_db"]
 
 
 async def connect_to_mongo():
-    await init_beanie(database=db, document_models=[User])
-    print("Connected to MongoDB")
+    await init_beanie(
+        database=db, document_models=[User, DoctorDetails, Appointment, Message]
+    )
+    print("Successfully Connected to MongoDB")
